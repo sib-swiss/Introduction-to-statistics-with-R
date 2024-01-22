93ccfabd9e4319289403fd949f7ddd1619e57027
jeitziner <rachel.jeitziner@epfl.ch>
2024-01-22 17:00:30 +0100

    Files

--- docs/assets/Scripts/Day1.R
+++ docs/assets/Scripts/Day1.R
@@ -1,7 +1,7 @@
 #-----------------
 #-----------------
 # Introduction to statistics with R
-# 6-9 February 2023
+# 2024
 # In Lausanne 
 #-----------------
 #-----------------
@@ -18,6 +18,7 @@ library(ISwR)
 data(hellung) 
 
 summary(hellung)
+
 glucose <- hellung$glucose
 mean(hellung$glucose)
 sd(hellung$glucose)
@@ -29,6 +30,7 @@ sd(hellung$diameter)
 means.hellung <- c()
 for (i in 1:3){
   means.hellung <- c(means.hellung, mean(hellung[,i]))
+  print(means.hellung)
 }
 means.hellung
 
@@ -39,16 +41,17 @@ for (i in 1:3){
 stdev.hellung
 
 par(mfrow=c(2,2)) # for viewing multiple plots (2 rows x 2 columns = 4 plots)
-hist(hellung$conc, main="a title", xlab="concentration", ylab="freq")
+hist(hellung$conc, main="Hellung data", xlab="concentration", ylab="freq")
 hist(hellung$diameter, col="red")
 hist(hellung$glucose)
 hist(hellung$conc, breaks=20)
 
 par(mfrow=c(1,2)) # for viewing multiple plots
-boxplot(conc ~ glucose, data=hellung)
+boxplot( conc ~ glucose,data=hellung)
 boxplot(diameter ~ glucose, data=hellung)
 boxplot(hellung$conc ~ hellung$glucose)
 boxplot(hellung$diameter ~ hellung$glucose)
+
 
 cor(hellung$conc, hellung$diameter)
 plot(diameter ~ conc, data=hellung) 
@@ -69,15 +72,16 @@ ggplot(hellung, aes(x=log(conc), y=diameter)) + geom_point()
 setwd("/Users/Rachel/Desktop/Introduction-to-statistics-with-R/docs/assets/exercises/")
 data <- read.csv("data.csv") 
 
+
 data
 summary(data)
 sd(data[,1]); sd(data[,2]); sd(data[,3])
 
 datatoplot <- data[,1]
-
+#pdf("datanumber1.pdf")
 ## Plot 4 rows of graphs on one plot
 par(mfrow=c(4,1))
-dev.off()
+
 # 1st plot: individual points on the x-axis; random noise on the
 #           y-axis so that points are not too much superimposed
 plot(datatoplot, runif( length(datatoplot), -1, 1), xlim=range(datatoplot))
@@ -92,6 +96,56 @@ arrows(mean(datatoplot)-sd(datatoplot), 0, mean(datatoplot)+sd(datatoplot), 0, a
 
 # 4th plot: boxplot
 boxplot(datatoplot, horizontal=TRUE, ylim=range(datatoplot))
+#dev.off()
+
+## now I am looking at the second column
+
+datatoplot <- data[,2]
+#pdf("datanumber2.pdf")
+## Plot 4 rows of graphs on one plot
+par(mfrow=c(4,1))
+
+# 1st plot: individual points on the x-axis; random noise on the
+#           y-axis so that points are not too much superimposed
+plot(datatoplot, runif( length(datatoplot), -1, 1), xlim=range(datatoplot))
+
+# 2nd plot: histogram, with the density line superimposed
+hist(datatoplot, xlim=range(datatoplot), breaks=20)
+lines(density(datatoplot))
+
+# 3rd plot: average +/- Sd
+plot(mean(datatoplot), 0, xlim=range(datatoplot), main="Mean and standard deviation of a")
+arrows(mean(datatoplot)-sd(datatoplot), 0, mean(datatoplot)+sd(datatoplot), 0, angle=90, code=3)
+
+# 4th plot: boxplot
+boxplot(datatoplot, horizontal=TRUE, ylim=range(datatoplot))
+#dev.off()
+
+## now I am looking at the third column
+
+datatoplot <- data[,3]
+#pdf("datanumber3.pdf")
+## Plot 4 rows of graphs on one plot
+par(mfrow=c(4,1))
+
+# 1st plot: individual points on the x-axis; random noise on the
+#           y-axis so that points are not too much superimposed
+plot(datatoplot, runif( length(datatoplot), -1, 1), xlim=range(datatoplot))
+
+# 2nd plot: histogram, with the density line superimposed
+hist(datatoplot, xlim=range(datatoplot), breaks=20)
+lines(density(datatoplot))
+
+# 3rd plot: average +/- Sd
+plot(mean(datatoplot), 0, xlim=range(datatoplot), main="Mean and standard deviation of a")
+arrows(mean(datatoplot)-sd(datatoplot), 0, mean(datatoplot)+sd(datatoplot), 0, angle=90, code=3)
+
+# 4th plot: boxplot
+boxplot(datatoplot, horizontal=TRUE, ylim=range(datatoplot))
+#dev.off()
+
+
+## Last exercise
 
 
 student <- read.table("students.csv",header=TRUE,sep=",")
@@ -108,7 +162,8 @@ student$smoker <- factor(student$smoker,levels=unique(student$smoker))
 student$leftrighthanded <- as.factor(as.character(student$leftrighthanded))
 summary(student)
 
-student[student[,"height"] ==1.77,"height"] <-177 
+student[student[,"height"] ==1.77,"height"] <-177
+
 plot(student$height,student$weight,col=student$gender)
 boxplot(height~gender,data=student)
 boxplot(weight~gender,data=student)
